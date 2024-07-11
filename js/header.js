b3a4840f79d36ca7fa4d8a7ff40a40d3abe05cd1
gijsdelaat <gijsdelaat1993@gmail.com>
2024-07-11 14:09:20 +0200

    v0.20 styling

--- js/header.js
+++ js/header.js
@@ -2,20 +2,45 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentPage = window.location.pathname.split("/").pop();
     
     const header = `
-    <div class="logo" style="display: flex; align-items: center;">
-    <a href="index.html" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
-        <img src="src/mobile-logo.svg" alt="Logo" style="height: 80px; margin-right: 10px;margin: 15px; ">
-        <h1>SpaarSlim</h1>
-    </a>
-</div>
-<nav>
-<ul>
-    <li><a href="box3sparen.html" class="${currentPage === 'box3sparen.html' ? 'active' : ''}"><i class="fas fa-piggy-bank"></i> Box 3 Sparen</a></li>
-    <li><a href="pensioensparen.html" class="${currentPage === 'pensioensparen.html' ? 'active' : ''}"><i class="fas fa-chart-line"></i> Pensioensparen</a></li>
-    <li><a href="bruto_netto.html" class="${currentPage === 'bruto_netto.html' ? 'active' : ''}"><i class="fas fa-exchange-alt"></i> Netto/Bruto Salaris Omrekenen</a></li>
-</ul>
-</nav>
+    <div class="logo">
+        <a href="index.html">
+            <img src="src/mobile-logo.svg" alt="Logo">
+            <h1>SpaarSlim</h1>
+        </a>
+    </div>
+    <nav>
+        <ul class="nav-list">
+            <li><a href="box3sparen.html" class="${currentPage === 'box3sparen.html' ? 'active' : ''}">Box 3 Sparen</a></li>
+            <li><a href="pensioensparen.html" class="${currentPage === 'pensioensparen.html' ? 'active' : ''}">Pensioensparen</a></li>
+            <li><a href="bruto_netto.html" class="${currentPage === 'bruto_netto.html' ? 'active' : ''}">Netto/Bruto</a></li>
+        </ul>
+    </nav>
+    <div class="contact">
+        <a href="mailto:gijsdelaat1993@gmail.com" class="contact-button">Contact</a>
+    </div>
+    <div class="mobile-menu-toggle">
+        <i class="fas fa-bars"></i>
+    </div>
     `;
     
     document.getElementById('header-placeholder').innerHTML = header;
+
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    mobileMenuToggle.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 768) {
+            mobileMenuToggle.style.display = 'block';
+            navList.style.display = 'none';
+        } else {
+            mobileMenuToggle.style.display = 'none';
+            navList.style.display = 'flex';
+        }
+    });
+
+    window.dispatchEvent(new Event('resize'));
 });
